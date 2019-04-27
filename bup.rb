ef378436a9653522a7916760f359ae321ae3fdf7
YuraKalistratov <50049828+YuraKalistratov@users.noreply.github.com>
2019-04-27 18:52:56 +0300

    Add files via upload

---
+++ bup.rb
@@ -0,0 +1,10 @@
+require 'yaml'
+
+config = YAML.load_file('C:\Users\admin\Documents\config_timing4.yml')
+a = "donetska"
+b = "ukrainska"
+a + b = "3 UAH"
+puts "Enter the station code, e.g. 'a' for donetska and 'b' for ukrainska, and for calculating the price please use the following formula: a + b"
+name = gets
+puts "3 UAH" if "a + b"
+
